5fddfa327d2b3ca6fba0318924d15316a472cea0
Daniela Rodríguez <114227863+DanaroNait2002@users.noreply.github.com>
2022-11-26 21:33:01 +0000

    Commit_11
    Creación de Items funcional

--- Assets/Scripts/Creation_Items.cs
+++ Assets/Scripts/Creation_Items.cs
@@ -4,22 +4,106 @@ using UnityEngine;
 
 public class Creation_Items : MonoBehaviour
 {
+    public enum StateSelector
+    {
+        Waiting,
+        SelectionMove,
+        Moving,
+        Realease,
+        SelectTypeCreation,
+        CreationTree01,
+        CreationTree02,
+        CreationBarn,
+        CreationFlower,
+    }
+    
     [SerializeField]
-    GameObject selectedItem;
+    StateSelector currentState = StateSelector.Waiting;
+
+    [SerializeField]
+    GameObject creationCanvas, selectedItem;
+
+    Vector2 mousePos;
 
     private void Update()
     {
-        if (selectedItem != null)
+        switch (currentState) 
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            //if (Physics.Raycast, out hit, Math.Infinity)
+            case StateSelector.Moving:
+                MoveItem();
+                break;
+
+            case StateSelector.SelectionMove:
+                SelectedItem();
+                break;
+
+            case StateSelector.Realease:
+                Realease(); 
+                break;
         }
     }
 
-    public void Create_Item(GameObject prefab)
+    public void CreatingItem(GameObject prefab)
     {
-        Instantiate(prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
-        prefab = selectedItem;
+        selectedItem = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+        currentState = StateSelector.Moving;
+    }
+
+    public void SelectedItem()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (hitInfo.collider.tag.Equals("Creation"))
+                {
+                    selectedItem = hitInfo.collider.gameObject;
+
+                    switch (currentState)
+                    {
+                        case StateSelector.SelectionMove:
+                            currentState = StateSelector.Moving;
+                            break;
+                    }
+
+                    Cursor.visible = false;
+                }
+            }
+        }
+    }
+
+    public void MoveItem()
+    {
+        Vector3 pos = Input.mousePosition;
+        Ray rayo = Camera.main.ScreenPointToRay(pos);
+        RaycastHit hitInfo;
+
+        selectedItem.SetActive(false);
+
+        if (Physics.Raycast(rayo, out hitInfo))
+        {
+            selectedItem.transform.position = hitInfo.point /*+ ((Vector3.up * selectedItem.transform.localScale.y) * 1.5f)*/;
+        }
+
+        selectedItem.SetActive(true);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentState = StateSelector.Realease;
+        }
+    }
+
+    public void Realease()
+    {
+        selectedItem = null;
+
+        currentState = StateSelector.Waiting;
+
+        Cursor.visible = true;
     }
 }
