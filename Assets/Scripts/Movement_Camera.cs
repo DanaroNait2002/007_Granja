0cde81a6692f5d65cb0a7897f17315800756e1c0
Daniela Rodríguez <danaronait2002@gmail.com>
2022-12-01 11:22:55 +0000

    Movimiento de Camara
    Funcional

--- Assets/Scripts/Movement_Camera.cs
+++ Assets/Scripts/Movement_Camera.cs
@@ -7,31 +7,24 @@ public class Movement_Camera : MonoBehaviour
     [SerializeField]
     GameObject infoIcon, info;
 
+    [SerializeField]
+    Vector3 oldPos;
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-
-            }
-            
-                Vector3 mousePos = Input.mousePosition;
-
-            Camera.main.transform.position = mousePos;
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                
-            }
-
+            Vector3 newPos = oldPos - Input.mousePosition;
+            Camera.main.transform.position += new Vector3(newPos.y, 0.0f, newPos.x) * Time.deltaTime;
         }
 
+        oldPos = Input.mousePosition;
 
         if (Input.GetMouseButtonUp(0))
         {
             LeanTween.scale(infoIcon, Vector3.zero, 0.25f);
             LeanTween.scale(info, Vector3.zero, 0.25f);
         }
+
     }
 }
